3674156d9e3dffc9b066939517ca76150dc58aca
jgargar558j <jgargar558j@g.educaand.es>
2025-05-24 11:29:57 +0200

    BrownOn -> brownOn

--- PI2025/template-webpack-ts/src/escenas/levels/level03.ts
+++ PI2025/template-webpack-ts/src/escenas/levels/level03.ts
@@ -7,7 +7,7 @@ export class Level03 extends LevelHandler {
     }
 
     create(): void {
-        this.createStage(Constants.MAPS.LEVELS.LEVEL_3.TILEMAP_JSON, Constants.MAPS.PLATFORM_LAYER, Constants.BACKGROUNDS.LEVEL_2);
+        this.createStage(Constants.MAPS.LEVELS.LEVEL_3.TILEMAP_JSON, Constants.MAPS.PLATFORM_LAYER, Constants.BACKGROUNDS.LEVEL_3);
 
         this.createEnemies([Constants.ENEMIES.BUNNY]) // , Constants.ENEMIES.BEE para añadir más enemigos
         this.createCollectables([Constants.OBJECTS.COLLECTABLES.FRUITS.APPLE, Constants.OBJECTS.COLLECTABLES.FRUITS.BANANA, Constants.OBJECTS.COLLECTABLES.FRUITS.CHERRY]);
